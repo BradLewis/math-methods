10eeff40cbea4b79379626eadff0e2271e163811
Bradley Lewis <blewis2109@gmail.com>
2018-08-10 17:37:18 -0400

    wrote function to get next iteration

--- runge-kutta/RungeKutta.scala
+++ runge-kutta/RungeKutta.scala
@@ -3,10 +3,22 @@ object RungeKutta {
         
     }
 
-    def rungeKutta(fy: (Double, Double) => Double, 
+    def rungeKutta(f_ty: (Double, Double) => Double,
+                    y0: Double,
+                    stepsize: Double) {
+        
+    }
+
+    def getNext(f_ty: (Double, Double) => Double, 
                     yn: Double, 
                     tn: Double,
                     stepsize: Double): Double {
-        k1 = stepsize * fy(tn, yn)
+        var h = stepsize
+        var k1 = h * f_ty(tn, yn)
+        var k2 = h * f_ty(tn + h / 2, yn + k1 / 2)
+        var k3 = h * f_ty(tn + h / 2, yn + k2 / 2)
+        var k4 = h * f_ty(tn + h, yn + k3)
+
+        yn + (1 / 6) * (k1 + 2 * k2 + 2 * k3 + k4)
     }
 }
